290cd22e918de45cffc048c43149c6c8eadd3bc2
Davide Cologgi <dcologgi@e4r7p3.42roma.it>
2023-03-07 15:48:22 +0100

    so_long

--- animation.c
+++ animation.c
@@ -6,7 +6,7 @@
 /*   By: dcologgi <dcologgi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 11:56:21 by dcologgi          #+#    #+#             */
-/*   Updated: 2023/03/07 15:28:47 by dcologgi         ###   ########.fr       */
+/*   Updated: 2023/03/07 15:47:20 by dcologgi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	animation_array(t_data *game)
 {
 	game->enemy_sprite[0] = mlx_xpm_file_to_image(game->mlx,
-				"./imgs/enemysprite1.xpm",&game->img_width, &game->img_height);
+			"./imgs/enemysprite1.xpm", &game->img_width, &game->img_height);
 	game->enemy_sprite[1] = mlx_xpm_file_to_image(game->mlx,
-				"./imgs/enemysprite2.xpm",&game->img_width, &game->img_height);
+			"./imgs/enemysprite2.xpm", &game->img_width, &game->img_height);
 	game->enemy_sprite[2] = mlx_xpm_file_to_image(game->mlx,
-				"./imgs/enemysprite3.xpm",&game->img_width, &game->img_height);
+			"./imgs/enemysprite3.xpm", &game->img_width, &game->img_height);
 	game->enemy_sprite[3] = mlx_xpm_file_to_image(game->mlx,
-				"./imgs/enemysprite4.xpm",&game->img_width, &game->img_height);
+			"./imgs/enemysprite4.xpm", &game->img_width, &game->img_height);
 }
 
 int	enemy_loop(t_data *game)
